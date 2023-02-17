bf18d27fd964264f71c039b30550c188babd6c55
mahendra-120994 <nirmalmahender5010@gmail.com>
2023-02-17 10:52:46 +0000

    created login page

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
+      <img
+        className="website-logo"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+        alt="website logo"
+      />
+      <ul className="nav-menu">
+        <li className="nav-link">Home</li>
+        <li className="nav-link">Products</li>
+        <li className="nav-link">Cart</li>
+      </ul>
+      <button type="button" className="logout-desktop-btn">
+        Logout
+      </button>
+      <button type="button" className="logout-mobile-btn">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+          alt="logout icon"
+          className="logout-icon"
+        />
+      </button>
+    </div>
+  </nav>
+)
+export default Header
